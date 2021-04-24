dd7de369cd1d2f3658d4588f79b1d5b773e0f35e
Andrey Kirichenko <andrey.kirichenko999@gmail.com>
2021-04-25 02:27:44 +0300

    added vk ui interface

--- src/App.js
+++ src/App.js
@@ -1,24 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {
+  AdaptivityProvider,
+  AppRoot,
+  ConfigProvider,
+  Group,
+  Panel,
+  PanelHeader,
+  Slider,
+  SplitCol,
+  SplitLayout,
+  useAdaptivity,
+  View,
+  ViewWidth,
 
-function App() {
+  FormLayout,
+  FormLayoutGroup,
+  FormItem,
+  Input,
+  ModalRoot,
+  ModalPage,
+  ModalPageHeader,
+  Separator,
+
+  CellButton,
+  Cell,
+  Placeholder,
+  Avatar,
+  Button,
+  usePlatform,
+  Alert,
+  FixedLayout,
+  PanelHeaderContent,
+  PanelHeaderButton,
+  PanelHeaderBack,
+  PanelHeaderContext,
+  List,
+  Div,
+  Select,
+  CustomSelectOption,
+} from '@vkontakte/vkui';
+
+import {
+  Icon56UsersOutline,
+  Icon56MentionOutline,
+  Icon56MessageReadOutline,
+  Icon28MessageOutline,
+  Icon28UsersOutline,
+  Icon24Done,
+  Icon28SettingsOutline,
+} from '@vkontakte/icons';
+
+// import { Line } from 'react-chartjs-2';
+
+import '@vkontakte/vkui/dist/vkui.css';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppRoot>
+      <View activePanel="main">
+        <Panel id="main">
+          <FixedLayout filled vertical="top">
+            <Div>
+              <Select
+                placeholder="Масса звезды" 
+                // options={getRandomUsers(10).map(user => ({ label: user.name, value: user.id, avatar: user.photo_100 }))}
+                renderOption={({ option, ...restProps }) => (
+                  <CustomSelectOption {...restProps} before={<Avatar size={24} src={option.avatar} />} />
+                )}
+              />
+            </Div>
+          </FixedLayout>
+          
+          <FixedLayout filled vertical="bottom"> 
+            <Div>
+              <Slider />
+            </Div>
+          </FixedLayout>
+        </Panel>
+      </View>
+    </AppRoot>
   );
 }
 
